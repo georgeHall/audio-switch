6a38c5bf2eb577c23f058c474bcafcfb7aaba54e
George Hall <georgeHall@users.noreply.github.com>
2021-07-24 08:45:46 +0100

    Setup Pins

--- code/pico/src/main.cpp
+++ code/pico/src/main.cpp
@@ -3,21 +3,30 @@
 
 const int col_0 = 1;
 const int col_1 = 2;
-
 const int row_0 = 3;
 const int row_1 = 4;
 
 const int led_col_0 = 1;
 const int led_col_1 = 2;
-
 const int led_row_0 = 3;
 const int led_row_1 = 4;
 
 
+const int input_jack = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(col_0, OUTPUT);
+  pinMode(col_1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void seupPins() {
+
+  pinMode(row_0, INPUT_PULLDOWN);
+  pinMode(row_1, INPUT_PULLDOWN);
+
 }

ea56d99b0e5cb0391a3a622bb8460d7515bc1f74
George Hall <georgeHall@users.noreply.github.com>
2021-07-24 08:31:05 +0100

    Added pico code

---
+++ code/pico/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+
+const int col_0 = 1;
+const int col_1 = 2;
+
+const int row_0 = 3;
+const int row_1 = 4;
+
+const int led_col_0 = 1;
+const int led_col_1 = 2;
+
+const int led_row_0 = 3;
+const int led_row_1 = 4;
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

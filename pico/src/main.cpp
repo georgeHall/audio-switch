c10962a94c47106616f468ac1adc09d6873c9674
George Hall <georgeHall@users.noreply.github.com>
2021-07-29 12:36:14 +0100

    Remove second switch case

--- pico/src/main.cpp
+++ pico/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-const int delay_in_micro_seconds = 100;
+const int delay_in_micro_seconds = 20;
 
 const int cols[] = {9, 10};
 const int rows[] = {6, 7};
@@ -35,7 +35,8 @@ void setup() {
   pinMode(select_b, OUTPUT);
 }
 
-void lightLed(inputJack jack) {
+void setPins(inputJack jack) {
+  // Set LED pins so no light is on.
   digitalWrite(led_row_0, HIGH);
   digitalWrite(led_row_1, HIGH);
   digitalWrite(led_col_0, LOW);
@@ -43,55 +44,51 @@ void lightLed(inputJack jack) {
 
   switch(jack) {
     case ONE:
+      // Activate LED 1
       digitalWrite(led_col_0, HIGH);
       digitalWrite(led_row_0, LOW);
+
+      // Set audio output to 1
+      digitalWrite(select_a, LOW);
+      digitalWrite(select_b,LOW);
       break;
     case TWO:
+      // Activate LED 2
       digitalWrite(led_col_0, HIGH);
       digitalWrite(led_row_1, LOW);
+
+      // Set audio output to 2
+      digitalWrite(select_a, HIGH);
+      digitalWrite(select_b, LOW);
       break;
     case THREE:
+      // Activate LED 3
       digitalWrite(led_col_1, HIGH);
       digitalWrite(led_row_0, LOW);
+
+      // Set audio output to 3
+      digitalWrite(select_a, LOW);
+      digitalWrite(select_b, HIGH);
       break;
     case FOUR:
+      // Activate LED 4
       digitalWrite(led_col_1, HIGH);
       digitalWrite(led_row_1, LOW);
+
+      // Set audio output to 3
+      digitalWrite(select_a, HIGH);
+      digitalWrite(select_b, HIGH);
       break;
     default:
       break;
   }
 }
 
-void setOutput(inputJack jack) {
-   switch(jack) {
-    case ONE:
-      digitalWrite(select_a, LOW);
-      digitalWrite(select_b, LOW);
-      break;
-    case TWO:
-      digitalWrite(select_a, HIGH);
-      digitalWrite(select_b, LOW);
-      break;
-    case THREE:
-      digitalWrite(select_a, LOW);
-      digitalWrite(select_b, HIGH);
-      break;
-    case FOUR:
-      digitalWrite(select_a, HIGH);
-      digitalWrite(select_b, HIGH);
-      break;
-    default:
-      break;
-   }
-}
-
 void loop() {
   for(int col : cols) {
       for(int row: rows) {
         if(digitalRead(row) == HIGH) {
-          setOutput(matrix[col][row]);
-          lightLed(matrix[col][row]);
+          setPins(matrix[col][row]);
         }
       }
       delay(delay_in_micro_seconds/length_of_cols);
